37b909e223a302382d87eda79f8465d97b770982
Morozok123 <102429172+Morozok123@users.noreply.github.com>
2025-10-05 13:18:49 +0300

    Update TestRunner.java

--- src/test/java/org/ibs/runners/TestRunner.java
+++ src/test/java/org/ibs/runners/TestRunner.java
@@ -1,18 +1,23 @@
 package org.ibs.runners;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
-        value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, pretty, html:target/cucumber-reports.html, json:target/cucumber.json")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "org.ibs.steps")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
+        value = "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm, " +
+                "pretty, " +
+                "html:target/cucumber-reports.html, " +
+                "json:target/cucumber-reports/cucumber.json, " +
+                "junit:target/cucumber-reports/cucumber.xml")
 public class TestRunner {
 }
